7fb7479011f2b18cfe20e7b986e7900af44173d3
GalahadP92 <ivan.preciadoc92@gmail.com>
2021-07-18 07:08:51 -0500

    Now Main scene is working with a sphere that shows the other person on the call

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,35 +8,68 @@ using agora_utilities;
 
 public class GameController : MonoBehaviour
 {
-  [SerializeField]
-  private string AppID = " ";
+    // Use this for initialization
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+    private ArrayList permissionList = new ArrayList();
+#endif
 
-  [SerializeField]
-  private GameObject videoSphere = null;
+    [SerializeField]
+    private string AppID = "aab8b8f5a8cd4469a63042fcfafe7063";
 
-  [SerializeField]
-  private string channelName = "Test1";
+    [SerializeField]
+    private GameObject videoSphere = null;
 
-  static VideoCall videoSystem = null;
+    [SerializeField]
+    private string channelName = "Test1";
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    videoSystem = new VideoCall();
-    if (videoSystem != null)
+    static VideoCall videoCallSystem = null;
+
+    void Awake()
     {
-      videoSystem.InitEngine(AppID);
-      videoSystem.SetSphereGameObject(videoSphere);
-      videoSystem.JoinChannel(channelName);
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+		permissionList.Add(Permission.Microphone);         
+		permissionList.Add(Permission.Camera);               
+#endif
     }
-  }
 
-  // Update is called once per frame
-  void Update()
-  {
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoCallSystem = new VideoCall();
+        if (videoCallSystem != null)
+        {
 
-  }
+            videoCallSystem.SetSphereGameObject(videoSphere);
+            videoCallSystem.InitEngine(AppID);
+            videoCallSystem.JoinChannel(channelName);
+        }
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        CheckPermissions();
+    }
 
+    void OnApplicationQuit()
+    {
+        if (videoCallSystem != null)
+        {
+            videoCallSystem.DestroyEngine();
+        }
+    }
+
+    private void CheckPermissions()
+    {
+#if (UNITY_2018_3_OR_NEWER && UNITY_ANDROID)
+        foreach(string permission in permissionList)
+        {
+            if (!Permission.HasUserAuthorizedPermission(permission))
+            {                 
+				Permission.RequestUserPermission(permission);
+			}
+        }
+#endif
+    }
 
 }
--- Assets/Scripts/VideoCall.cs
+++ Assets/Scripts/VideoCall.cs
@@ -8,119 +8,132 @@ using agora_utilities;
 
 public class VideoCall
 {
-  // instance of agora engine
-  private IRtcEngine m_RtcEngine;
-  private Text MessageText;
-  [SerializeField]
-  GameObject videoSphere = null;
+    // instance of agora engine
+    private IRtcEngine m_RtcEngine;
+    private Text MessageText;
+
+    [SerializeField]
+    GameObject videoSphere = null;
 
 
-  public void InitEngine(string appID)
-  {
-    //  Start the RTC Engine
-    if (m_RtcEngine != null)
+    public void InitEngine(string appID)
     {
-      Debug.Log("An instance of the engine has been started already. Please unload it first!");
-      return;
+        //  Start the RTC Engine
+        if (m_RtcEngine != null)
+        {
+            Debug.Log("An instance of the engine has been started already. Please unload it first!");
+            return;
+        }
+
+        m_RtcEngine = IRtcEngine.GetEngine(appID);
+        Debug.Log("Succesfully loaded engine");
+
+        //  Set up flags for debugging/logging
+        m_RtcEngine.SetLogFilter(LOG_FILTER.DEBUG | LOG_FILTER.INFO | LOG_FILTER.WARNING | LOG_FILTER.ERROR | LOG_FILTER.CRITICAL);
+
     }
 
-    m_RtcEngine = IRtcEngine.GetEngine(appID);
-    Debug.Log("Succesfully loaded engine");
-
-    //  Set up flags for debugging/logging
-    m_RtcEngine.SetLogFilter(LOG_FILTER.DEBUG | LOG_FILTER.INFO | LOG_FILTER.WARNING | LOG_FILTER.ERROR | LOG_FILTER.CRITICAL);
-
-  }
-
-  public void DestroyEngine()
-  {
-    Debug.Log("calling unloadEngine");
-
-    // delete
-    if (m_RtcEngine != null)
+    public void DestroyEngine()
     {
-      IRtcEngine.Destroy();  // Place this call in ApplicationQuit
-      m_RtcEngine = null;
-    }
-  }
+        Debug.Log("calling unloadEngine");
 
-  public string GetSdkVersion()
-  {
-    string ver = IRtcEngine.GetSdkVersion();
-    return ver;
-  }
-
-  public int JoinChannel(string channelName)
-  {
-    Debug.Log("Joining channel: " + channelName);
-
-    if (m_RtcEngine == null)
-    {
-      return 0;
+        // delete
+        if (m_RtcEngine != null)
+        {
+            IRtcEngine.Destroy();  // Place this call in ApplicationQuit
+            m_RtcEngine = null;
+        }
     }
 
-    //  Setting callbacks. This could probably be handled better using dedicated functions
-    m_RtcEngine.OnJoinChannelSuccess = onJoinChannelSucces;
-    m_RtcEngine.OnUserJoined = onUserJoinedChannel;
-    //m_RtcEngine.OnUserOffline = Callback to handle user offline;
-    //m_RtcEngine.OnWarning = Callback To handle Warnings;
-    //m_RtcEngine.OnError = Callback to handle errors;
-
-    // enable video
-    m_RtcEngine.EnableVideo();
-    // allow camera output callback
-    m_RtcEngine.EnableVideoObserver();
-
-    return m_RtcEngine.JoinChannel(channelName, null, 0);
-  }
-
-  public void LeaveChannel()
-  {
-    if (m_RtcEngine == null)
-      return;
-
-    // leave channel
-    m_RtcEngine.LeaveChannel();
-    // deregister video frame observers in native-c code
-    m_RtcEngine.DisableVideoObserver();
-  }
-
-  //  Callback functions for the event handler
-  private void onJoinChannelSucces(string channelName, uint channelID, int timeElapsed)
-  {
-    Debug.Log("Succesfully joined: " + channelName + ". ID: " + channelID);
-  }
-
-  private void onUserJoinedChannel(uint uid, int timeElapsed)
-  {
-    Debug.Log("User " + uid + " joined the channel.");
-
-    //  Maybe spawn an object to render the view 
-    VideoSurface videoSurface = SetVideoOnSphere();
-    if (!ReferenceEquals(videoSurface, null))
+    public string GetSdkVersion()
     {
-      // configure videoSurface
-      videoSurface.SetForUser(uid);
-      videoSurface.SetEnable(true);
-      videoSurface.SetVideoSurfaceType(AgoraVideoSurfaceType.RawImage);
-      videoSurface.SetGameFps(30);
-    }
-  }
-
-  public VideoSurface SetVideoOnSphere()
-  {
-    if (videoSphere == null)
-    {
-      return null;
+        string ver = IRtcEngine.GetSdkVersion();
+        return ver;
     }
 
-    videoSphere.AddComponent<RawImage>();
-    VideoSurface sphereVideoSurface = videoSphere.AddComponent<VideoSurface>();
-    return sphereVideoSurface;
-  }
+    public int JoinChannel(string channelName)
+    {
+        Debug.Log("Joining channel: " + channelName);
 
-  public void SetSphereGameObject(GameObject _sphere)
-  {
-    videoSphere = _sphere;
-  }
+        if (m_RtcEngine == null)
+        {
+            return 0;
+        }
+
+        //  Setting callbacks. This could probably be handled better using dedicated functions
+        m_RtcEngine.OnJoinChannelSuccess = onJoinChannelSucces;
+        m_RtcEngine.OnUserJoined = onUserJoinedChannel;
+        //m_RtcEngine.OnUserOffline = Callback to handle user offline;
+        //m_RtcEngine.OnWarning = Callback To handle Warnings;
+        //m_RtcEngine.OnError = Callback to handle errors;
+
+        // enable video
+        m_RtcEngine.EnableVideo();
+        // allow camera output callback
+        m_RtcEngine.EnableVideoObserver();
+
+        return m_RtcEngine.JoinChannel(channelName, null, 0);
+    }
+
+    public void LeaveChannel()
+    {
+        if (m_RtcEngine == null)
+            return;
+
+        // leave channel
+        m_RtcEngine.LeaveChannel();
+        // deregister video frame observers in native-c code
+        m_RtcEngine.DisableVideoObserver();
+    }
+
+    //  Callback functions for the event handler
+    private void onJoinChannelSucces(string channelName, uint channelID, int timeElapsed)
+    {
+        Debug.Log("Succesfully joined: " + channelName + ". ID: " + channelID);
+    }
+
+    private const float Offset = 100;
+    private void onUserJoinedChannel(uint uid, int timeElapsed)
+    {
+        Debug.Log("User " + uid + " joined the channel.");
+
+        if (videoSphere == null)
+        {
+            return;
+        }
+        videoSphere.name = uid.ToString();
+        //videoSphere.AddComponent<RawImage>();
+        videoSphere.transform.Rotate(0f, 0.0f, 180.0f);
+
+        VideoSurface sphereVideoSurface = videoSphere.AddComponent<VideoSurface>();
+    
+
+        if (!ReferenceEquals(sphereVideoSurface, null))
+        {
+            // configure videoSurface
+            sphereVideoSurface.SetForUser(uid);
+            sphereVideoSurface.SetEnable(true);
+            sphereVideoSurface.SetVideoSurfaceType(AgoraVideoSurfaceType.Renderer);
+            sphereVideoSurface.SetGameFps(30);
+
+        }
+    }
+
+    public VideoSurface SetVideoOnSphere(string goName)
+    {
+        if (videoSphere == null)
+        {
+            return null;
+        }
+
+        videoSphere.name = goName;
+        videoSphere.AddComponent<RawImage>();
+        VideoSurface sphereVideoSurface = videoSphere.AddComponent<VideoSurface>();
+        return sphereVideoSurface;
+    }
+
+    public void SetSphereGameObject(GameObject _sphere)
+    {
+        videoSphere = _sphere;
+    }
 }
